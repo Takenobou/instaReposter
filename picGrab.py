9ca87a02a5a0d1794148acffcaf1d9f65f31f20e
Takenobou <31248629+Takenobou@users.noreply.github.com>
2019-07-14 19:59:06 +0100

    Add files via upload
    Changed up the file a bit for use

---
+++ picGrab.py
@@ -0,0 +1,54 @@
+import os
+import PIL
+import praw
+import requests
+from PIL import Image
+
+#reddit image grabber, resizer and downloader
+
+
+
+def main():
+        clID = ""
+        clSE = ""
+        User = ""
+        Password = ""
+        reddit = praw.Reddit(client_id= clID, 
+        client_secret = clSE,
+        username = User,
+        password = Password,
+        user_agent = 'picGrabV1')
+
+        
+        subreddit = reddit.subreddit('memes') #target subreddit
+        d = os.getcwd()
+        ImagePath = d + "\\Images" #image path
+        hot_python = subreddit.hot(limit= 12) #amount of images to be downloaded (10 + 2 stickies)
+
+        for submission in hot_python:
+            if not submission.stickied and not submission.over_18:
+                print (submission.author)
+                print (submission.url)
+                print (hot_python)
+                submission.upvote()
+                imageName = str(submission.author)
+                print (imageName)
+                img_data = requests.get(submission.url).content
+                with open(ImagePath + "\\" + imageName + '.jpg', 'wb') as handler: #writing the image
+                    handler.write(img_data)
+                
+                #resizing the image for instagram
+
+                with Image.open(ImagePath + "\\" + imageName + '.jpg') as img:
+                    width, height = img.size
+                if  width > 1080 or height > 1080:
+                    baseheight = 1080
+                    img = Image.open(ImagePath + "\\" + imageName + '.jpg')
+                    hpercent = (baseheight / float(img.size[1]))
+                    wsize = int((float(img.size[0]) * float(hpercent)))
+                    img = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
+                    img.save(ImagePath + "\\" + imageName+ '.jpg')
+                    
+
+if __name__ == "__main__":
+    print("PicGrab imported")
